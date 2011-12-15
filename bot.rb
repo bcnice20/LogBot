973c57d37d1049081ad874b1dac6e003e8c25204
Brad Carter <bcart3r@gmail.com>
2011-12-15 16:52:48 -0500

    initial commit

---
+++ bot.rb
@@ -0,0 +1,15 @@
+require "cinch"
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server "irc.freenode.net"
+    c.channels ["#sinatra"]
+    c.nick "bcart3r"
+  end
+  
+  on :message, /^(.+)/ do |m|
+    File.open("log.txt", "a") do |f|
+      f.write "#{m.user}: #{m.message} (#{Time.now.asctime}) \n"
+    end
+  end
+end
